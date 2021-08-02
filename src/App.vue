0c5c8ac8ac731f408cfff461e6e337c318cedbed
Aung Ko Ko Khant <aungkokokhant16@gmail.com>
2021-08-02 18:11:36 +0630

    git cli small project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,69 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>hi there</h1>
+  
+  <div v-if="showModal"> 
+      <Modal :theme="theme" @closeModal="showModal=false"> 
+
+        <h1>Login success </h1>
+        <p>Welcome Aung Ko Ko Khant</p>
+
+        <!-- <template v-slot:links>
+          <a href="">Sing Up</a>
+          <a href="">Register</a>
+        </template> -->
+
+      </Modal>
+
+      
+  </div>
+
+  <div v-if="showModal1"> 
+      <Modal :theme1="theme1" @closeModal="showModal1=false"> 
+
+        <h1>Login Failure </h1>
+        <p>Pls try again</p>
+
+        
+
+      </Modal>
+
+      
+  </div>
+
+ 
+
+  <button @click="showModal=true">open modal</button>
+  <button @click="showModal1=true">close modal</button>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Modal from "./components/Modal.vue"
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+
+  data(){
+
+    return{
+    header:"loggin success",
+    content:"Welcom Aung Ko Ko Khant",
+    theme:"success",
+    theme1:"danger",
+    showModal:false,
+    showModal1:false,
+   
+    }
+
+    
+  },
+  components:{
+    Modal
+    
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  h1{
+    color: blue;
+  }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal" :class="{success:theme==='success',danger:theme1==='danger'}">
+        
+      <slot/>
+      <!-- <slot name="links"/> -->
+      
+      
+    </div>
+
+    
+  </div>
+</template>
+
+<script>
+export default {
+    props:['header','content','theme','theme1'],
+    methods:{
+      closeModal(){
+        this.$emit("closeModal")
+      }
+    }
+}
+</script>
+
+<style scoped>
+  .backdrop{
+    position: fixed;
+    top: 0;
+    background: rgba(0, 0, 0, .5);
+    width: 100%;
+    height: 100%;
+  }
+  .modal{
+    width: 400px;
+    padding: 20px;
+    margin: 100px auto;
+    background-color: white;
+    border-radius: 10px;
+  }
+  .success{
+      background-color: green;
+      color: white;
+  }
+  .danger{
+      background-color: crimson;
+      color: white;
+  }
+</style>
